351c0f9060d337df75c921caf136f950a7ea6198
farhadopal7111 <164672904+farhadopal7111@users.noreply.github.com>
2024-04-14 19:38:40 +0500

    currency excchange

---
+++ main.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer"
+
+const currency : any ={
+   USD: 1,
+   EUR: 0.91,
+   GBP: 0.76,
+   INR: 75,
+   PKR: 285
+};
+
+let user_answer = await inquirer.prompt([
+   
+    { 
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ['USD','EUR', 'GBP', 'INR', 'PKR']
+},
+
+   { 
+
+    name: "to",
+    message: "enter to currency",
+    type: "list",
+    choices: ['USD','EUR', 'GBP', 'INR', 'PKR']
+},
+{ 
+    name: "amount",
+    message: "enter your amount",
+    type: "number",
+   
+}
+]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount =  currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log (convertedAmount);
